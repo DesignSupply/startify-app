f123ef4d44fd0a800db353c39587fb01316a6439
DesignSupply <ogawa@designsupply-web.com>
2025-02-23 15:42:20 +0900

    [mod] コンポーネント構造の変更

---
+++ backend/laravel/resources/views/components/navigation.blade.php
@@ -0,0 +1,52 @@
+<nav class="app-navigation">
+    @if (
+        !str_contains(Route::currentRouteName(), 'password-reset')
+        && !str_contains(Route::currentRouteName(), 'admin.password-reset')
+        && (
+            !str_contains(Route::currentRouteName(), 'signup')
+            || in_array(Route::currentRouteName(), ['signup', 'signup.pending'])
+        )
+    )
+        <a href="{{ route('frontpage') }}">フロントページ</a>
+    @endif
+    @if (
+        !str_contains(Route::currentRouteName(), 'password-reset')
+        && !str_contains(Route::currentRouteName(), 'signup')
+        && Route::currentRouteName() !== 'signin'
+        && !str_contains(Route::currentRouteName(), 'admin')
+    )
+        @if (Auth::check())
+            <a href="{{ route('home') }}">ホーム</a>
+            <form method="POST" action="{{ route('signout.post') }}" style="display: inline;">
+                @csrf
+                <button type="submit">ログアウト</button>
+            </form>
+        @else
+            <a href="{{ route('signin') }}">ログイン</a>
+        @endif
+    @endif
+    @if (
+        Route::currentRouteName() === 'frontpage'
+        || (str_contains(Route::currentRouteName(), 'admin')
+        && Route::currentRouteName() !== 'admin'
+        && !str_contains(Route::currentRouteName(), 'admin.password-reset'))
+    )
+        @if (Auth::guard('admin')->check())
+            <a href="{{ route('admin.dashboard') }}">管理者ダッシュボード</a>
+            <form method="POST" action="{{ route('admin.signout.post') }}" style="display: inline;">
+                @csrf
+                <button type="submit">管理者ログアウト</button>
+            </form>
+        @else
+            <a href="{{ route('admin') }}">管理者ログイン</a>
+        @endif
+    @endif
+    @if (
+        !str_contains(Route::currentRouteName(), 'admin')
+        && !str_contains(Route::currentRouteName(), 'password-reset')
+        && !str_contains(Route::currentRouteName(), 'signup')
+        && !Auth::check()
+    )
+        <a href="{{ route('signup') }}">新規ユーザー登録</a>
+    @endif
+</nav>
