b3dede10062dbb582dc6faa236a555ca2c7405ca
DesignSupply <ogawa@designsupply-web.com>
2025-05-12 01:33:34 +0900

    [add] 共通コンポーネント作成

--- frontend/next/src/app/layout.tsx
+++ frontend/next/src/app/layout.tsx
@@ -1,4 +1,7 @@
 import '@/styles/globals.css';
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+import OffCanvas from '@/components/OffCanvas';
 
 export default function RootLayout({
   children,
@@ -9,8 +12,11 @@ export default function RootLayout({
     <html lang="ja">
       <body>
         <div className="app-layout">
+          <Header />
           {children}
+          <Footer />
         </div>
+        <OffCanvas />
       </body>
     </html>
   );
---
+++ frontend/next/src/components/OffCanvas.tsx
@@ -0,0 +1,5 @@
+export default function OffCanvas() {
+  return (
+    <div className="app-offcanvas">オフキャンバス要素</div>
+  );
+} 
