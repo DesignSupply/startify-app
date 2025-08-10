fe905e91e92e791fc51af0cd0dbe0d49a0d7ed71
DesignSupply <ogawa@designsupply-web.com>
2025-08-10 23:26:17 +0900

    [update] 主要ライブラリのサンプル追加

--- frontend/vite/src/ts/main.ts
+++ frontend/vite/src/ts/main.ts
@@ -1,6 +1,44 @@
 import '../styles/tailwind.css';
 import '../scss/style.scss';
 import $ from 'jquery';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+// GSAP
+gsap.registerPlugin(ScrollTrigger);
+gsap.fromTo('.js-gsap-animation', {
+  opacity: 0,
+  y: 32,
+}, {
+  opacity: 1,
+  y: 0,
+  duration: 1,
+  ease: 'power2.inOut',
+  scrollTrigger: {
+    trigger: '.js-gsap-animation',
+    start: 'top 50%',
+    end: 'bottom 50%',
+    scrub: 1,
+  },
+});
+
+// Swiper
+const swiper = new Swiper('.swiper', {
+  modules: [Navigation, Pagination],
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
 
 // jQuery
 $(function () {
