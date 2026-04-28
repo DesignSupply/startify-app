02efe785c13c0a1d07890d5e187fae76ee3d8d3e
DesignSupply. <ogawa@designsupply-web.com>
2026-04-28 13:56:52 +0900

    [mod] カスタム用スタイルシート命名変更

--- backend/wordpress/wp-content/themes/startify-classic-theme/functions/actions.php
+++ backend/wordpress/wp-content/themes/startify-classic-theme/functions/actions.php
@@ -3,7 +3,7 @@
   // StyleSheet読み込み
   function register_stylesheet() {
     wp_enqueue_style('style-css', get_template_directory_uri().'/style.css', array(), null, 'all');
-    wp_enqueue_style('main-css', get_template_directory_uri().'/assets/css/main.min.css', array(), null, 'all');
+    wp_enqueue_style('theme-default-css', get_template_directory_uri().'/assets/css/style.min.css', array(), null, 'all');
   }
   add_action('wp_enqueue_scripts', 'register_stylesheet');
 
